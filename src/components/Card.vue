f5c8b4405b43bf2985dea412af34eb3ddbcc6f35
Ahmed-SD <ahmedadam.abduallah@gmail.com>
2022-04-03 19:20:06 -0700

    first commit

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,76 +1,70 @@
-<template>	
-  <div id="cardId">	
-    <v-row>	
-      <v-col	
-      cols="4"	
-      >	
-        <v-img	
-        :src="require('../../api/storage/app/public/'+post.image)"
-        id="postImage"	
-        :to="{name: 'singlePost'}"	
+<template>
+  <div id="cardId overflow-hidden">
+    <v-row>
+      <v-col cols="4">
+        <v-img
+          src="../assets/logo.png"
+          id="postImage"
+          :to="{ name: 'singlePost' }"
         >
-        </v-img>	
-      </v-col>	
-      <v-col	
-      cols="8"	
-      >	
-      <router-link id="titleId" color="white" :to="{name: 'singlePost', params: { post: post}}">{{post.id}}</router-link><br>	
-      <v-card-text id="textContent" class="font-weight-regular">
-         {{this.post.postContent}}	
-
-     </v-card-text>	
-      </v-col>	
-    </v-row>	
-  </div>	
-</template>	
+        </v-img>
+      </v-col>
+      <v-col cols="8">
+        <router-link
+          id="titleId"
+          color="white"
+          :to="{ name: 'singlePost', params: { post: post } }"
+          >{{ post.title }}</router-link
+        ><br />
+        <v-card-text id="textContent" class="font-weight-regular">
+          {{ post.content }}
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </div>
+</template>
 
 <script>
 export default {
-  setup() {
-    
-  },
-  data: () => ({
+  setup() {},
+  data: () => ({}),
 
-  }),
-  
-  props: ['post']
-  
-}
+  props: ["post"],
+};
 </script>
 
-<style scoped>	
-#postImage{	
-  margin: 2px 0px -2px 2px;	
-  height: 195px;	
-  border-radius: 17px  0px 0px 17px;	
-}	
-#cardId{	
-  margin: 5px;	
-  height: 200px;	
-  border: 1px solid gray;	
-  border-radius: 20px;	
-}	
-#socailIcons{	
-  margin-top: 15px;	
-}	
-#avatar{	
-  margin-left: 9px;	
-}	
-@media only screen and (max-width: 600px){	
-		
-}	
-span{	
-  font-size: 20px;	
-}	
-#titleId{	
-  font-size: 20px;	
-  text-decoration: none;	
-  color:#006c6e;	
-  margin-left: 50px;	
-}	
-#textContent{	
-  color:black;	
-  text-align: right;	
-  padding-top: 8px !important;	
-}	
-</style> 	
+<style scoped>
+#postImage {
+  margin: 2px 0px -2px 2px;
+  height: 195px;
+  border-radius: 17px 0px 0px 17px;
+}
+#cardId {
+  margin: 5px;
+  height: 200px;
+  border: 1px solid gray;
+  border-radius: 20px;
+}
+#socailIcons {
+  margin-top: 15px;
+}
+#avatar {
+  margin-left: 9px;
+}
+@media only screen and (max-width: 600px) {
+}
+span {
+  font-size: 20px;
+}
+#titleId {
+  font-size: 20px;
+  text-decoration: none;
+  color: #006c6e;
+  margin-left: 50px;
+}
+#textContent {
+  color: black;
+  text-align: right;
+  padding-top: 8px !important;
+}
+</style>
--- src/layouts/header/AppBar.vue
+++ src/layouts/header/AppBar.vue
@@ -1,7 +1,12 @@
 <template>
   <v-layout>
     <v-app-bar id="appBar" density="compact" fixed>
-      <img height="30" width="0" src="../../../../logo5.svg" id="logo">
+      <img
+        height="30"
+        width="0"
+        src="../../assets/brand/Untitled-2.png"
+        id="logo"
+      />
       <v-spacer></v-spacer>
       <v-row>
         <v-col id="searchColId" cols="2" sm="1" md="1" lg="1" xl="1">
@@ -9,145 +14,136 @@
         </v-col>
         <v-col cols="10" sm="5" md="5" lg="5" xl="5">
           <div id="search" class="mt-5 p-5">
-          <v-text-field @focus="x=false" @blur="x=true">
-        </v-text-field>
-        </div>
+            <v-text-field @focus="x = false" @blur="x = true"> </v-text-field>
+          </div>
         </v-col>
-         <v-col cols="5" sm="2" md="2" lg="2" xl="2">
-           <v-btn
+        <v-col cols="5" sm="2" md="2" lg="2" xl="2">
+          <v-btn
             id="loginButtonId"
             class="mt-6 ml-10"
             color="#006c6e"
-            :to="{name: 'login'}"
-            ><span>تسجيل</span></v-btn>
-         </v-col>
-         <v-col cols="6" sm="2" md="2" lg="2" xl="2">
-        <v-btn
-          id="registerButtonId"
-          class="mt-6"
-          color="#006c6e"
-          :to="{name: 'register'}"
-        >
-        <v-icon id="registerIconId" color="white" left>mdi-handshake-outline</v-icon>
+            :to="{ name: 'login' }"
+            ><span>تسجيل</span></v-btn
+          >
+        </v-col>
+        <v-col cols="6" sm="2" md="2" lg="2" xl="2">
+          <v-btn
+            id="registerButtonId"
+            class="mt-6"
+            color="#006c6e"
+            :to="{ name: 'register' }"
+          >
+            <v-icon id="registerIconId" color="white" left
+              >mdi-handshake-outline</v-icon
+            >
 
-        <span>انضم الينا</span>
-      </v-btn>
-      <v-col class="searchColIdMini" cols="2" sm="1" md="1" lg="1" xl="1">
-          <i id="iconId" class="fas fa-search" v-if="x"></i>
+            <span>انضم الينا</span>
+          </v-btn>
+          <v-col class="searchColIdMini" cols="2" sm="1" md="1" lg="1" xl="1">
+            <i id="iconId" class="fas fa-search" v-if="x"></i>
+          </v-col>
+          <v-col class="searchColIdMini" cols="10" sm="5" md="5" lg="5" xl="5">
+            <div id="search" class="mt-5 p-5">
+              <v-text-field @focus="x = false" @blur="x = true"> </v-text-field>
+            </div>
+          </v-col>
         </v-col>
-        <v-col class="searchColIdMini" cols="10" sm="5" md="5" lg="5" xl="5">
-          <div id="search" class="mt-5 p-5">
-          <v-text-field @focus="x=false" @blur="x=true">
-        </v-text-field>
-        </div>
-        </v-col>
-         </v-col>
       </v-row>
-     
-      
-    
     </v-app-bar>
-
-    
   </v-layout>
 </template>
 
-
-
 <script>
-  export default {
-    data: () => ({
-      x: true,
-      items: [
-        { title: 'Click Me' },
-        { title: 'Click Me' },
-        { title: 'Click Me' },
-        { title: 'Click Me 2' },
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    x: true,
+    items: [
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me 2" },
+    ],
+  }),
+};
 </script>
 
 <style scoped>
-#appBar{
+#appBar {
   padding: 0px !important;
   height: 55px !important;
 }
-#appBar *{
+#appBar * {
   margin-bottom: 4px !important;
 }
-#logo{
+#logo {
   width: 150px !important;
   height: 150px !important;
   margin-top: 10px;
 }
- #iconId {
+#iconId {
   color: rgb(165, 161, 161);
   margin-top: 40px;
   position: absolute;
 }
-span{
+span {
   color: white;
 }
-.searchColIdMini{
+.searchColIdMini {
   display: none;
 }
-@media only screen and (max-width: 1000px){
-	#loginButtonId{
+@media only screen and (max-width: 1000px) {
+  #loginButtonId {
     margin-left: 0px !important;
   }
 }
-@media only screen and (max-width: 700px){
-	#registerButtonId{
+@media only screen and (max-width: 700px) {
+  #registerButtonId {
     margin-left: 10px !important;
   }
-
 }
-@media only screen and (max-width: 600px){
-	#registerButtonId{
+@media only screen and (max-width: 600px) {
+  #registerButtonId {
     margin: 0px !important;
     margin-bottom: 90px !important;
   }
-  #loginButtonId{
+  #loginButtonId {
     margin: 0px !important;
   }
-  #searchColId{
+  #searchColId {
     display: none !important;
   }
-  #logo{
+  #logo {
     margin-top: 16px;
   }
-  .searchColIdMini{
+  .searchColIdMini {
     margin-top: -100px;
     display: inline;
     position: absolute;
     margin-left: -160px;
   }
-  #appBar{
+  #appBar {
     height: 120px !important;
   }
-  #searchColId{
+  #searchColId {
     display: none !important;
   }
-  #iconId{
+  #iconId {
     margin-top: 28px !important;
   }
 }
-@media only screen and (max-width: 370px){
-#registerIconId{
-  display: none;
+@media only screen and (max-width: 370px) {
+  #registerIconId {
+    display: none;
+  }
+  #registerButtonId {
+    width: 80px;
+  }
+  #loginButtonId {
+    width: 20px !important;
+  }
+  #logo {
+    margin-left: -60px !important;
+    margin-right: 0px !important;
+  }
 }
-#registerButtonId{
-  width: 80px;
-}
-#loginButtonId{
-  width: 20px !important;
-}
-#logo{
-  margin-left: -60px !important;
-  margin-right: 0px !important;
-}
-}
-
-
 </style>
